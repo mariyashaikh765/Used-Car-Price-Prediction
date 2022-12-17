c5978b2c755f2ca20a37cfe93302d2df9689332d
Shaikh Mariya <92854911+mariyashaikh765@users.noreply.github.com>
2022-12-17 16:42:46 +0530

    Update predict_page.py

--- predict_page.py
+++ predict_page.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pickle
 import pandas as pd
-import matplotlib.pyplot as plt
 import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
